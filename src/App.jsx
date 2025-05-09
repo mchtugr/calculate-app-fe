6c268a8700a8fcb13a5b57557f4113fd74e95ad3
MUCUGUR <mucahit.ugur@mercedes-benz.com>
2025-05-09 10:36:34 +0300

    feat: init calculate app

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [ numbers, setNumbers ] = useState({
+    first: '',
+    second: ''
+  })
+  const [ result, setResult ] = useState(null);
+
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (!isNaN(value)) {
+
+      setNumbers((prev) => ({
+        ...prev,
+        [ name ]: value
+      }))
+      return
+    }
+  }
+
+  const onRecalculate = () => {
+    setResult(null)
+    setNumbers({
+      first: '',
+      second: ''
+    })
+  }
+
+  const onAddNumbers = (e) => {
+    e.preventDefault()
+
+    const { first, second } = numbers
+
+    fetch('http://localhost:3000/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        first: parseFloat(first),
+        second: parseFloat(second)
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // alert(`The sum is ${ data.sum }`)
+        setResult(data.sum)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  return (
+    <>
+      <h1>Calculate App</h1>
+      { !result && <form onSubmit={ onAddNumbers }>
+        <div className="input-container">
+          <input autoFocus name="first" type="text" value={ numbers.first } onChange={ onInputChange } placeholder="Enter a number" />
+          <input name="second" type="text" value={ numbers.second } onChange={ onInputChange } placeholder="Enter a number" />
+        </div>
+        <button className='calculate-btn' disabled={ !numbers.first || !numbers.second } type="submit">Show Sum</button>
+      </form> }
+      { result && <div className="result-container">
+        <h2>Result</h2>
+        <p> <b>{ numbers.first }</b> + <b>{ numbers.second }</b> equals to <b>{ result }</b></p>
+        <button className='calculate-btn' type="button" onClick={ onRecalculate }>Calculate again</button>
+      </div> }
+    </>
+  )
+}
+
+export default App
